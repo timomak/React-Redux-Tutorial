52cb4ad34ba553e4a5f652b683f16e28a86b93bd
timo <tmakhlay2@gmail.com>
2020-12-06 15:34:15 -0800

    Finished counter tutorial

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,24 +1,18 @@
-// import logo from "./logo.svg";
-// import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counterReducer);
+  const isLogged = useSelector((state) => state.isLoggedReducer);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {!isLogged ? <h3>Secret info</h3> : null}
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 
 const store = createStore(
   allReducers,
-  window.__REDUX_DEVTOOLS_EXTENTION__ && window.__REDUX_DEVTOOLS_EXTENTION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
